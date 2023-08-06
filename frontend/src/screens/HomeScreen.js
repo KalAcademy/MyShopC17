4013285e46a02c26f497eaa22c8b0c4557ce1b95
Kal <info@kalacademy.org>
2023-08-06 11:03:41 -0700

    Integrating frontend with backend

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,9 +1,16 @@
-import React from 'react'
+import React, {useEffect} from 'react'
 import {Col, Row} from 'react-bootstrap'
-import products from '../products'
+import axios from 'axios'
 import Product from '../components/Product'
 
 const HomeScreen = () => {
+
+  useEffect(() => {
+    const fetchProducts = async () => {
+      const {data} = await axios.get('/api/products')
+    }
+  })
+
   return (
     <>
       <h1>Latest Products</h1>
