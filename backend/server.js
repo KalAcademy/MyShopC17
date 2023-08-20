fc7f5cc01f90827876f165b81a578c822d5fffa8
Kal <info@kalacademy.org>
2023-08-20 11:04:51 -0700

    models and routes

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,13 @@
 import express from 'express'
-import products from './data/products'
+import dotenv from 'dotenv'
+import products from './data/products.js'
+import productRoutes from './routes/productRoutes.js'
+import connectDB from './config/db.js'
 
 const app = express()
-app.get('/api/products', (req, res) => {
-  res.json(products)
-})
+dotenv.config()
+connectDB()
 
-app.get('/api/product/:id', (req, res) => {
-  const product = products.find(p => p._id === req.params.id )
-  res.json(product)
-})
+app.use('/api/products', productRoutes)
 
 app.listen(5000, console.log('Server is running on port 5000.'))
