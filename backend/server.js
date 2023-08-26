f9d8357318f59d27646e00dd5e53253e89d4283d
Kal <info@kalacademy.org>
2023-08-26 11:01:34 -0700

    Error handler, routes and redux toolkit

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,8 @@
 import express from 'express'
 import dotenv from 'dotenv'
-import products from './data/products.js'
 import productRoutes from './routes/productRoutes.js'
 import connectDB from './config/db.js'
+import errorHandler from './middleware/errorMiddleware.js'
 
 const app = express()
 dotenv.config()
@@ -10,4 +10,5 @@ connectDB()
 
 app.use('/api/products', productRoutes)
 
+app.use(errorHandler)
 app.listen(5000, console.log('Server is running on port 5000.'))
